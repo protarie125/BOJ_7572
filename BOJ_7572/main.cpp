32be4dc4daf0b65858ca056294520b9f8f97d538
protarie125 <protarie@gmail.com>
2023-12-01 19:23:20 +0900

    초기 커밋.

---
+++ BOJ_7572/main.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <bit>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+using vs = vector<string>;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	cin >> n;
+
+	n -= 2013;
+
+	n %= 60;
+	if (n < 0) {
+		n += 60;
+	}
+
+	const auto a = n % 12;
+	const auto b = n % 10;
+
+	cout << (char)('A' + ((a + 5) % 12)) << (b + 9) % 10;
+
+	return 0;
+}
